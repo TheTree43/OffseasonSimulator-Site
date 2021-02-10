c8eb0450ada63d75d4216f345d89ff135e93a465
Sam Root <sjroot@gmail.com>
2021-02-09 20:13:33 -0600

    Flowchart up and running

--- OffseasonSimulator/urls.py
+++ OffseasonSimulator/urls.py
@@ -3,16 +3,15 @@
 from django.contrib import admin
 from django.urls import path
 from. import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns = [
     path('', views.home, name='home' ),
     path('about', views.about, name='about' ),
     path('Packers', views.Packers, name='Packers' ),
     path('Bears', views.Bears, name='Bears' ),
     path('Vikings', views.Vikings, name='Vikings' ),
     path('Lions', views.Lions, name='Lions' )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- flowchart/admin.py
+++ flowchart/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Flowchart
+
+admin.site.register(Flowchart)
