1f6d7520dc76088cc81026ef7daafbd6dd150e1b
Sam Root <sjroot@gmail.com>
2021-02-11 22:21:14 -0600

    Flowcharts are working!

--- flowchart/models.py
+++ flowchart/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Flowchart(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    team = models.CharField(max_length=40)
     date = models.DateTimeField()
--- flowchart/views.py
+++ flowchart/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-
+from .models import Flowchart
 # Create your views here.
 def Packers(request):
-    return render(request, 'flowchart/Packers.html')
+    flowchart = Flowchart.objects
+    return render(request, 'flowchart/Packers.html', {'flowchart':flowchart})
 
 def Bears(request):
-    return render(request, 'flowchart/Bears.html')
+    flowchart = Flowchart.objects
+    return render(request, 'flowchart/Bears.html', {'flowchart':flowchart})
 
 def Vikings(request):
-    return render(request, 'flowchart/Vikings.html')
+    flowchart = Flowchart.objects
+    return render(request, 'flowchart/Vikings.html', {'flowchart':flowchart})
 
 def Lions(request):
-    return render(request, 'flowchart/Lions.html')
+    flowchart = Flowchart.objects
+    return render(request, 'flowchart/Lions.html', {'flowchart':flowchart})
