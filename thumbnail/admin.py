4e51cf04f7c931e759a907d970c2d0e5491544da
Sam Root <sjroot@gmail.com>
2021-02-12 01:12:14 -0600

    yep

--- thumbnail/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Thumbnail
-
-admin.site.register(Thumbnail)
--- thumbnail/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Thumbnail
-
-def about(request):
-    thumbnails = Thumbnail.objects
-    return render(request, 'thumbnail/about.html', {'thumbnails':thumbnails})
